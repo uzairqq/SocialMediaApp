e3a0ca00178d3a730581527772ee2cf8d4f3625b
Uzair Iqbal <uzair.qq@outlook.com>
2023-05-02 17:05:40 +0500

    Pasted the Tailwind Confiq Code. Deleted Src Folder and Created New Files.

--- peopleshare_front_jsm/src/App.js
+++ peopleshare_front_jsm/src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
